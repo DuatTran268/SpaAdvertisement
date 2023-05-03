455005dd679b225da38f009be9773cfdfa1e05fc
DuatTran268 <duatsen36@gmail.com>
2023-05-04 00:14:24 +0700

    fix get api service by name

--- src/SpaCenter/SpaCenter.Services/Manages/Services/ServiceRepository.cs
+++ src/SpaCenter/SpaCenter.Services/Manages/Services/ServiceRepository.cs
@@ -161,6 +161,10 @@ namespace SpaCenter.Services.Manages.Services
                     || st.UrlSlug.Contains(query.ServiceSlug)
                     );
                 }
+                if (!string.IsNullOrWhiteSpace(query.Name))
+                {
+                    serviceQuery = serviceQuery.Where(s => s.Name == query.Name);
+                }
                 if (!string.IsNullOrWhiteSpace(query.ServiceSlug))
                 {
                     serviceQuery = serviceQuery.Where(s => s.UrlSlug == query.ServiceSlug);
