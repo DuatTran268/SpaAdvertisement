6c760e84291752ab0d0e63028aaf2e9b785a0d35
DuatTran268 <duatsen36@gmail.com>
2023-05-06 01:24:56 +0700

    feature share facebook social

--- src/spa-react/src/components/servicepost/ServicePost.js
+++ src/spa-react/src/components/servicepost/ServicePost.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from "react";
-import Dpostservice from "../../data/Dpostservice";
-import { Button, Image } from "react-bootstrap";
+import { Image } from "react-bootstrap";
 import "./ServicePost.scss";
 import { Link, useParams } from "react-router-dom";
 import Header from "../shared/Header";
 import Footer from "../shared/Footer";
-import { isEmptyOrSpaces } from "../../Utils/Utils";
 import { getServiceTypeBySlug } from "../../api/ServiceApi";
+import { FacebookShareButton } from "react-share";
+import { FacebookOutlined } from "@mui/icons-material";
 
 const ServicePost = () => {
   const params = useParams();
@@ -18,16 +18,14 @@ const ServicePost = () => {
   // : `https://localhost:7024/${serviceType.imageUrl}`;
 
   const getImage = (path) => {
-
-    console.log(path)
+    console.log(path);
     if (!path) {
       // set default image
       return `https://placehold.co/200x200?text=Image-not-found`;
-    } 
+    }
 
     return `https://localhost:7024/${path}`;
-  }
-
+  };
 
   useEffect(() => {
     document.title = "Chi tiết dịch vụ";
@@ -59,15 +57,23 @@ const ServicePost = () => {
             <h5 className="text-success">{serviceType.shortDescription}</h5>
             <p>{serviceType.description}</p>
 
-            <div className="text-end">
-              <Link to={`/service/booking`}>
-                <div className="btn btn-success">
-                    Book now
+            <div>
+              <FacebookShareButton
+                url={"https://dlu.edu.vn/"}
+                quote={"Hello everyone"}
+                hashtag="#spateam17"
+              >
+                <div className="text-primary">
+                  Chia sẻ ngay <FacebookOutlined size={32} round />
                 </div>
-              </Link>
-
+              </FacebookShareButton>
             </div>
 
+            <div className="text-end">
+              <Link to={`/service/booking`}>
+                <div className="btn btn-success">Book now</div>
+              </Link>
+            </div>
             <div className="text-center mt-5">
               <Link
                 className="text-success text-decoration-none"
