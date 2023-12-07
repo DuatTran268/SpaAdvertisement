ae8a08666c02fd651b779b6ae0781ccbc2208b88
Duat Tran <duatsen36@gmail.com>
2023-12-07 09:56:04 +0700

    test

--- src/SpaCenter/SpaCenter.Services/Manages/Roles/RoleRepository.cs
+++ src/SpaCenter/SpaCenter.Services/Manages/Roles/RoleRepository.cs
@@ -20,8 +20,8 @@ namespace SpaCenter.Services.Manages.Roles
         // get role not required
         public async Task<IList<RoleItem>> GetRoleNotRequired(CancellationToken cancellationToken = default)
         {
-            IQueryable<Role> departments = _context.Set<Role>();
-            return await departments.OrderBy(d => d.Name).Select(d => new RoleItem()
+            IQueryable<Role> user = _context.Set<Role>();
+            return await user.OrderBy(d => d.Name).Select(d => new RoleItem()
             {
                 Id = d.Id,
                 Name = d.Name,
--- src/spa-react/src/data/Dproductdetails.js
+++ src/spa-react/src/data/Dproductdetails.js
@@ -7,6 +7,8 @@ const Dproductdetail = [
     image: "https://gaspa.vn/wp-content/uploads/2022/06/Body-ngay-scaled.jpeg",
     description: "KEM CHỐNG NẮNG EFFCTIVE SUNCREEN SPF 50/PA+++ là sản phẩm kem chống nắng đến từ thương hiệu C13 Beauty, giúp chống lại tác hại của tia UV & bụi mịn tối ưu dưới mọi điều kiện sinh hoạt, kể cả thời tiết khắc nghiệt nhất. Sản phẩm đặc biệt phù hợp với mọi loại da.",
     imageUrl: "https://saigonsmilespa.com.vn/FileUpload/Images/lamtrangda_.jpg",
+
+    // abc
   }
 ]
 
